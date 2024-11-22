35f3889f6a8bb5883d7ac6d5dda84cb357d32a10
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-11-22 13:32:50 +0300

    get a single product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,6 +3,7 @@ const mongodb = require("mongodb");
 
 const db = require("../db");
 const Decimal128 = mongodb.Decimal128;
+const ObjectId = mongodb.ObjectId;
 
 const router = Router();
 
@@ -68,6 +69,7 @@ router.get("/", (req, res, next) => {
   // }
   const products = [];
   db.getDb()
+    .db()
     .collection("products")
     .find()
     .forEach((productDoc) => {
@@ -85,8 +87,18 @@ router.get("/", (req, res, next) => {
 
 // Get single product
 router.get("/:id", (req, res, next) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+  db.getDb()
+    .db()
+    .collection("products")
+    .findOne({ _id: new ObjectId(req.params.id) })
+    .then((productDoc) => {
+      productDoc.price = productDoc.price.toString();
+      res.status(200).json(productDoc);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "An error occurred" });
+    });
 });
 
 // Add new product
@@ -100,6 +112,7 @@ router.post("", (req, res, next) => {
   };
   console.log(newProduct);
   db.getDb()
+    .db()
     .collection("products")
     .insertOne(newProduct)
     .then((result) => {
